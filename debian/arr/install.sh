41bedd855c65ccc66f146a6d5d5c2b5923813b96
FireIP <44668877+FireIP@users.noreply.github.com>
2025-05-11 10:29:33 +0200

    Use install script instead of dockerfile

---
+++ debian/arr/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+cd /home/container
+mkdir opt
+chown container:container opt
+mkdir -p var/lib
+chown -R container:container var
+
+chmod +x /home/container/script/unattended-servarr.sh
+./script/unattended-servarr.sh -l -p -r -d
+rm ./script/unattended-servarr.sh
+
+curl -o install-sonarr.sh https://raw.githubusercontent.com/Sonarr/Sonarr/v5-develop/distribution/debian/install.sh
+chmod +x install-sonarr.sh
+./install-sonarr.sh -u --user container --group container
+rm install-sonarr.sh
+rm *tar.gz
+mv /opt/Sonarr opt/Sonarr
+mv /var/lib/sonarr/ var/lib/sonarr/
+
+apt autoremove -y && \
+apt-get clean \
+&& rm -rf /var/lib/apt/lists/* /var/cache/apt/archives/*
