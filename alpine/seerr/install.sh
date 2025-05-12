255f51ab7553a67d393e47763a594d5493f786b1
FireIP <44668877+FireIP@users.noreply.github.com>
2025-05-12 21:04:17 +0200

    fix install script and startup

--- alpine/seerr/install.sh
+++ alpine/seerr/install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 git clone https://github.com/Fallenbagel/jellyseerr.git
-ch /home/container/jellyseerr
+cd /home/container/jellyseerr
 git checkout main
 
 CYPRESS_INSTALL_BINARY=0 pnpm install --frozen-lockfile
 
 pnpm build
+
+mkdir -p /mnt/server
+mv /home/container/jellyseerr /mnt/server/jellyseerr
